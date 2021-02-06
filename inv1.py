64fec31648a27d45799914a0d2a789b9d7d15ee8
Uva337 <66168613+Uva337@users.noreply.github.com>
2021-02-06 08:10:52 +0300

    Update inv1.py

--- inv1.py
+++ inv1.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
-# -*- config: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-# Написать программу, которая считывает текст из файла и определяет, сколько в нем слов,
-# состоящих из не менее чем семи букв.
+#   Написать программу, которая считывает текст из файла и определяет, сколько в нем слов,
+#   состоящих из не менее чем семи букв.
 
 if __name__ == '__main__':
     with open('text.txt', 'r') as f:
         text = f.read()
-        words = []
-        k = 0
-        m = 0
-        w = ''
-        for i in text:
-            if i.isalpha() :
-                m += 1
-                w += i
-            else:
-                if w != '' :
-                    words.append(w)
-                if m > 6 :
-                    k += 1
-                m = 0
-                w = ''
-words = [i for i in words if len(i) > 6]
-print(*words, sep = '\n')
-print(len(words))
+    text = text.replace(".", " ")
+    text = text.replace(",", " ")
+    text = text.replace(";", " ")
+    text = text.replace("!", " ")
+    text = text.replace("?", " ")
+    sentences = text.split(" ")
+
+    for i in text:
+        text.replace(i, ' ')
+    sentences = [i for i in sentences if len(i) > 6]
+
+    print(f'{sentences}')
+    print(f'Число слов, состоящих из 7 букв: {len(sentences)}')
+    
